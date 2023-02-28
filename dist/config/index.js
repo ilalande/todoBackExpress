92955de33460cad79696b9c7efd415a0fada485e
Juliane <juliane@user-agency.com>
2023-02-28 15:12:38 +0100

    CRU(D) roads to tasks in Prisma

--- dist/config/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = void 0;
-require('dotenv').config();
-exports.config = {
-    PORT: process.env.PORT,
-};
